dc3a499df40b0ae0dc5f3aa3947b6990bbba898a
Oliver Loftus <o@oloft.us>
2014-03-27 00:23:41 +0000

    Minor updates to translate service

--- app/lingua-web/src/main/webapp/javascript/modules/controller/translateController.js
+++ app/lingua-web/src/main/webapp/javascript/modules/controller/translateController.js
@@ -3,29 +3,32 @@ define([ "util/commonTypes", "appRoot", "util/util" ], function() {
     var translateController = function($scope, translateService, languageNamesService, sourceLang, targetLang,
             initQuery) {
 
-        $scope.sourceLang = sourceLang;
-        $scope.targetLang = targetLang;
-        $scope.query = initQuery;
+        $scope.model = {};
+        $scope.func = {};
+        
+        $scope.model.sourceLang = sourceLang;
+        $scope.model.targetLang = targetLang;
+        $scope.model.query = initQuery;
 
-        $scope.doTranslate = function() {
+        $scope.func.doTranslate = function() {
 
-            var translationRequest = new TranslationRequest($scope.sourceLang, $scope.targetLang, $scope.query);
+            var translationRequest = new TranslationRequest($scope.model.sourceLang, $scope.model.targetLang, $scope.model.query);
             translateService.translate(translationRequest, function(data) {
-                $scope.translations = {
+                $scope.model.translations = {
                     google : data.translations.Google
                 };
             });
         };
 
-        languageNamesService.lookup(new LanguageNameRequest($scope.sourceLang), function(data) {
-            $scope.sourceLangName = data.langName;
+        languageNamesService.lookup(new LanguageNameRequest($scope.model.sourceLang), function(data) {
+            $scope.model.sourceLangName = data.langName;
         });
 
-        languageNamesService.lookup(new LanguageNameRequest($scope.targetLang), function(data) {
-            $scope.targetLangName = data.langName;
+        languageNamesService.lookup(new LanguageNameRequest($scope.model.targetLang), function(data) {
+            $scope.model.targetLangName = data.langName;
         });
 
-        $scope.doTranslate();
+        $scope.func.doTranslate();
     };
 
     linguaApp.controller("translateController", [ "$scope", "translateService", "languageNamesService", "sourceLang",
