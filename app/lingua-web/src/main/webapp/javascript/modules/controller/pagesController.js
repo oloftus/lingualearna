3b54a5a110acb8086e8843cb9555dc165696e45e
Oliver Loftus <o@oloft.us>
2014-04-27 13:52:59 +0100

    Improved draggable dialog

--- app/lingua-web/src/main/webapp/javascript/modules/controller/pagesController.js
+++ app/lingua-web/src/main/webapp/javascript/modules/controller/pagesController.js
@@ -4,14 +4,17 @@
 
     define(dependencies, function(linguaApp) {
 
-        linguaApp.controller("pagesController", [ "$scope", "$state", function($scope, $state){
+        linguaApp.controller("pagesController", [ "$scope", "$state", function($scope, $state) {
 
             $(".lingua-draggable-dialog").draggable({
-                handle : ".lingua-dialog-header"
+                handle : ".lingua-dialog-header",
+                containment : "document",
+                scroll: false
             });
-            
+
             $scope.$on("$viewContentLoaded", function(blah) {
-                console.log(blah);
+                $(".lingua-draggable-dialog .view:empty").parent().hide();
+                $(".lingua-draggable-dialog .view:not(:empty)").parent().show();
             });
 
             $state.go(AppStates.MAIN);
