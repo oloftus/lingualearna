b75b22a866ab1d07d1c452fd94377d975a771d23
Oliver Loftus <o@oloft.us>
2014-05-01 23:36:28 +0100

    Moved/renamed several files (+ associated packages) Deleted unnecessary files

--- app/lingua-web/src/main/java/com/lingualearna/web/controller/PagesController.java
+++ app/lingua-web/src/main/java/com/lingualearna/web/controller/PagesController.java
@@ -1,36 +1,14 @@
 package com.lingualearna.web.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller("/app")
 public class PagesController {
-
-    @RequestMapping("/note")
-    public String addNotePage() throws Exception {
-
-        return "addNote";
-    }
-
-    @RequestMapping("/note/{noteId}")
-    public ModelAndView editNotePage(@PathVariable int noteId) throws Exception {
-
-        ModelAndView mav = new ModelAndView("editNote");
-        mav.addObject(noteId);
-        return mav;
-    }
 
     @RequestMapping("/login")
     public String loginPage() throws Exception {
 
         return "login";
-    }
-
-    @RequestMapping("/translate")
-    public String translatePage() throws Exception {
-
-        return "translate";
     }
 }
