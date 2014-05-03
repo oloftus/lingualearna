e702c9e92b2a4e8fa44c25a2d55b0c4d0a9321e9
Oliver Loftus <o@oloft.us>
2014-05-03 23:48:17 +0100

    Added overlay to prevent user interaction on/before login

--- app/lingua-web/src/main/webapp/javascript/modules/controller/pageController.js
+++ app/lingua-web/src/main/webapp/javascript/modules/controller/pageController.js
@@ -5,55 +5,104 @@
     define(dependencies, function(linguaApp, ngRegistrationHelper) {
 
         var CSRF_TOKEN_NAME = "X-CSRF-TOKEN";
+        var linguaReaderName = "lingua-reader";
+        var dialogName = "lingua-main-dialog";
+        var overlayName = "lingua-overlay";
+        var closeButtonName = "lingua-dialog-close-main";
 
-        var doLoginAndCsrf = function(jsonWebService, $state, commsPipe) {
+        var manageLoginOverlay = function($scope) {
+
+            $scope.$on("$stateChangeSuccess", function(event, toState, toParams, fromState, fromParams) {
+
+                $overlay = $("#" + overlayName);
+                $closeButton = $("#" + closeButtonName);
+                $linguaReader = $("#" + linguaReaderName);
+                var $dialog = $("#" + dialogName);
+
+                if (toState.name === AppStates.LOGIN) {
+                    
+                    if ($overlay.length == 0) {
+                        $dialog.before("<div id='" + overlayName + "'></div>");
+                    }
+                    $closeButton.hide();
+                    $linguaReader.css({
+                        "height" : "100%"
+                    });
+                }
+                else {
+                    
+                    $overlay.remove();
+                    $closeButton.show();
+                    $linguaReader.css({
+                        "height" : "auto"
+                    });
+                }
+            });
+        };
+
+        var doLoginAndCsrf = function(jsonWebService, $state, commsPipe, $scope) {
+
+            manageLoginOverlay($scope);
 
             jsonWebService.executeSimple(Properties.csrfTokenApiUrl, function(data, status) {
-                
+
                 linguaApp.httpProvider.defaults.headers.common[CSRF_TOKEN_NAME] = data;
                 $state.go(AppStates.MAIN);
             }, function(data, status) {
 
                 if (status === HttpHeaders.PSEUDO_CSRF_NOT_PERMITTED) {
                     commsPipe.subscribe(Components.LOGIN, Components.PAGE, function(message) {
-                        doLoginAndCsrf(jsonWebService, $state, commsPipe);
+                        doLoginAndCsrf(jsonWebService, $state, commsPipe, $scope);
                     });
                     $state.go(AppStates.LOGIN);
                 }
             });
         };
+        
+        var makeDialogsDraggable = function() {
+            
+            var $dialog = $("#" + dialogName);
 
-        var setupDialogs = function($scope) {
-
-            var dialogName = "#lingua-main-dialog";
-
-            $(dialogName).draggable({
+            $dialog.draggable({
                 handle : ".lingua-dialog-header",
                 containment : "document",
                 scroll : false
             });
+        };
+        
+        var enableDialogToggle = function($scope) {
+            
+            var $dialog = $("#" + dialogName);
 
             $scope.$on("$viewContentLoaded", function() {
 
-                var leftOffset = (($(window).width() - $(dialogName).outerWidth()) / 2) + $(window).scrollLeft();
+                var $window = $(window);
+                var leftOffset = (($window.width() - $dialog.outerWidth()) / 2) + $window.scrollLeft();
 
-                $(dialogName + " .lingua-dialog-view:empty").parent().hide();
-                $(dialogName + " .lingua-dialog-view:not(:empty)").parent().show();
-                $(dialogName).css({
+                $dialog.find(".lingua-dialog-view:empty").parent().hide();
+                $dialog.find(".lingua-dialog-view:not(:empty)").parent().show();
+                $dialog.css({
                     "left" : leftOffset + "px",
                     "top" : "70px"
                 });
             });
         };
 
+        var setupDialogs = function($scope) {
+
+            makeDialogsDraggable();
+            enableDialogToggle($scope);
+        };
+
         var PageController = function($scope, $state, jsonWebService, commsPipe) {
 
             $scope.Properties = Properties;
             setupDialogs($scope);
-            doLoginAndCsrf(jsonWebService, $state, commsPipe);
+            doLoginAndCsrf(jsonWebService, $state, commsPipe, $scope);
         };
 
         ngRegistrationHelper(linguaApp).registerController("pageController",
                 [ "$scope", "$state", "jsonWebService", "commsPipe", PageController ]);
     });
 })();
+;
