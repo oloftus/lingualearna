f20a3c8eff718955317538b50185817b44da95fb
Oliver Loftus <o@oloft.us>
2014-05-26 00:26:05 +0100

    Moved common functionality into AbstractController

---
+++ app/lingua-web/src/main/java/com/lingualearna/web/controller/AbstractController.java
@@ -0,0 +1,17 @@
+package com.lingualearna.web.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.lingualearna.web.security.User;
+import com.lingualearna.web.service.UserService;
+
+public class AbstractController {
+
+    protected User getUser(UserService userService, Authentication authentication) {
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        User user = userService.getUserByUsername(userDetails.getUsername());
+        return user;
+    }
+}
